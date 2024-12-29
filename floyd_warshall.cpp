94d533fd232ae87310220b6b2389800185b5a29a
01fe23bcs120 <01fe23bcs120@kletech.ac.in>
2024-12-29 13:03:54 +0530

    Update floyd_warshall.cpp

--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -1,43 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <limits>
-using namespace std;
-
-const int INF = numeric_limits<int>::max();
-
-void floydWarshall(vector<vector<int>>& graph) {
-    int V = graph.size();
-    vector<vector<int>> dist = graph;
-
-    // Floyd-Warshall Algorithm
-    for (int k = 0; k < V; ++k) {
-        for (int i = 0; i < V; ++i) {
-            for (int j = 0; j < V; ++j) {
-                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][j] > dist[i][k] + dist[k][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-
-    // Output the shortest paths
-    for (const auto& row : dist) {
-        for (int val : row) {
-            if (val == INF) cout << "INF ";
-            else cout << val << " ";
+void print(int dist[][vert])
+{
+    for(int i=0; i<vert; i++)
+    {
+        for(int j=0; j<vert; j++)
+        {
+            if(dist[i][j]==INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-int main() {
-    vector<vector<int>> graph = {
-        {0, 3, INF, 7},
-        {8, 0, 2, INF},
-        {5, INF, 0, 1},
-        {2, INF, INF, 0}
-    };
+void floyd(int dist[][vert])
+{
+    for(int k=0; k<vert; k++)
+    {
+        for(int i=0; i<vert; i++)
+        {
+            for(int j=0; j<vert; j++)
+            {
+                if(dist[i][j]>dist[i][k]+dist[k][j] && dist[i][k]!=INF && dist[k][j]!=INF)
+                    dist[i][j]=dist[i][k]+dist[k][j];
+            }
+        }
+    }
+    print(dist);
+}
 
-    floydWarshall(graph);
-    return 0;
+void fw_5()
+{
+    int graph[vert][vert] = {{0,INF,3,INF},{2,0,INF,INF},{INF,7,0,1},{6,INF,INF,0}};
+    cout << "All pair shortest path between shelters and disaster zone : \n";
+    floyd(graph);
 }
